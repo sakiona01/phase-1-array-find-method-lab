eed948dec34d7389426f6e3e00cb5930df9c5482
Shawna Akiona <shawna.akiona@gmail.com>
2021-08-13 15:04:08 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+// const superBowlWin = record.find(record => record.result === "W");
+
+const superbowlWin = (record) => {
+   if (record.find((dub) => dub.result === 'W')){
+     return record.find((dub) => dub.result === 'W').year
+   }
+ }
+
+
+// function superBowlWin() {
+//    record.find(year);
+// }
+// const result = record.find(function(superBowlWin) {
+//    return record.startsWith('W');
+// });
